59167915486ead4946f7e422af4070ff19a42a3c
Theophilus <101416039+Theophilus06@users.noreply.github.com>
2022-04-19 12:42:47 +0100

    Add files via upload

---
+++ guess game updated.py
@@ -0,0 +1,14 @@
+secret = 9
+guess_count = 0
+guess_limit = 3
+while guess_count<guess_limit:
+    guess = int(input('guess '))
+    guess_count+=1
+    if guess == secret:
+        print('you won')
+        break
+    else:
+        print('try again')
+
+else:
+    print('you loss')
